ec4728321785f8c441b8eef5978a1600c00180a6
Nestor Valles <nestor@vlue.company>
2023-08-14 15:57:47 -0500

    hoverAnimation_debugging

--- src/app/hoverAnimations.js
+++ src/app/hoverAnimations.js
@@ -1,7 +1,7 @@
 import gsap from 'gsap';
 
 export function buttonsAnimation() {
-  let buttons = document.querySelectorAll('.button');
+  let buttons = document.querySelectorAll('.button .submit-button');
 
   buttons.forEach((button) => {
     button.addEventListener('mouseenter', () => {
