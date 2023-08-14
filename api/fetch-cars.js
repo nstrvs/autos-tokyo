26fa67007c125c2970eb2e2cb9f63ce7f392b480
Nestor Valles <nestor@vlue.company>
2023-08-14 14:08:41 -0500

    handle CORS

--- api/fetch-cars.js
+++ api/fetch-cars.js
@@ -3,6 +3,11 @@ const fetch = require('node-fetch');
 const ApiKey = process.env.WEBFLOW;
 
 module.exports = (req, res) => {
+  
+  res.setHeader('Access-Control-Allow-Origin', 'https://autostokyo.webflow.io');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
   const url = 'https://api.webflow.com/collections/64c5869d5fc3ada54bf96c88/items';
   const options = {
     method: 'GET',
