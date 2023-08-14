ddc95b15b9c01fa442b451d58accc7f7c38dde29
Nestor Valles <nestor@vlue.company>
2023-08-14 13:49:02 -0500

    bugs fixed

--- server/server.js
+++ server/server.js
@@ -5,16 +5,9 @@ const apiRoutes = require('./routes/api');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Enable CORS for all routes
 app.use(cors());
-
-// Parse JSON body
 app.use(express.json());
-
-// Serve static files from the 'dist' directory
-app.use(express.static('dist'));
-
-// Use the API routes defined in 'api.js'
+app.use(express.static('../dist'));
 app.use('/api', apiRoutes);
 
 app.listen(port, () => {
