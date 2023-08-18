d2a97e490698a30e39f7c9374cf1eb02c48cdbf9
Nestor Valles <nestor@vlue.company>
2023-08-18 15:20:43 -0500

    mobileMenu debugging

--- src/app/mobileMenu.js
+++ src/app/mobileMenu.js
@@ -1,13 +1,14 @@
 export function mobileMenu() {
   const openMenu = document.getElementById('open-menu');
   const closeMenu = document.getElementById('close-menu');
+  const mobileMenu = document.getElementById('mobile__menu');
 
   openMenu.addEventListener('click', (e) => {
-    closeMenu.classList.toggle('is-closed');
+    mobileMenu.classList.toggle('is-closed');
   });
 
   closeMenu.addEventListener('click', (e) => {
-    closeMenu.classList.toggle('is-closed');
+    mobileMenu.classList.toggle('is-closed');
   });
 
 };
