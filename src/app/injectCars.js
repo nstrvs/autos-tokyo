b53f732c6997aa707097bec06816d52a8d9a73e6
Nestor Valles <nestor@vlue.company>
2023-08-18 13:42:02 -0500

    injectCars debugging

--- src/app/injectCars.js
+++ src/app/injectCars.js
@@ -1,9 +1,9 @@
 export function injectCars() {
 
-  function createDiv(carName) {
+  function createDiv(carName, slug) {
     let vehicleSelection = document.getElementById('form__cars-list');
     const newDiv = document.createElement('label');
-    newDiv.id = carName;
+    newDiv.id = slug;
     newDiv.classList.add("radio-button-field", "showcars", "w-radio");
     newDiv.setAttribute('for', `${carName}-radio`);
     newDiv.innerHTML = `
@@ -11,6 +11,29 @@ export function injectCars() {
       <span class="radio-button-label w-form-label">${carName}</span>
     `
     vehicleSelection.appendChild(newDiv);
+  }
+
+  const images = [];
+
+  function createImage(carName, featuredImage) {
+    const newDiv = document.createElement('figure');
+    newDiv.classList.add('car-image__wrapper');
+    newDiv.innerHTML = `<img src="${featuredImage}" alt="${carName}">`;
+    document.body.appendChild(newDiv);
+  }
+
+  function hoverListener(carName, slug, featuredImage) {
+
+    const car = document.getElementById(slug);
+
+    car.addEventListener('mouseover', (e) => {
+        createImage(carName, featuredImage);
+    });
+
+    car.addEventListener('mouseout', (e) => {
+      const carImage = document.querySelector('.car-image__wrapper');
+      carImage.remove();
+    });
   }
 
   fetch('https://autos-tokyo.vercel.app/api/fetch-cars')
@@ -21,7 +44,18 @@ export function injectCars() {
 
       for (let i = 0; i < items.length; i++) {
         const carName = items[i].name;
+        const slug = items[i].slug;
+        const featuredImage = items[i]['front-page-image'].url;
+
+        
+        const img = new Image();
+        img.src = featuredImage;
+
+        // Add the preloaded image to the images array
+        images.push(img);
+
         createDiv(carName);
+        hoverListener(carName, slug, featuredImage);
       }
     })
     .catch((err) => console.error('error:' + err));
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import './index.css';
 import { employmentStatus, addIncome } from './app/form.js';
 import { coBuyer, coBuyerEmploymentStatus, coBuyerAddIncome, removeDiv } from './app/coBuyer.js';
-import { showCars } from './app/showCars';
 import { buttonsAnimation } from './app/hoverAnimations';
 import { injectCars } from './app/injectCars';
 
 
 employmentStatus();
 addIncome();
-showCars();
 buttonsAnimation();
 injectCars();
 
