a70664f8a25ca055808ad5f291c1f8b3f26324eb
Nestor Valles <nestor@vlue.company>
2023-08-23 16:20:58 -0600

    swiper debugging

--- src/app/swiper.js
+++ src/app/swiper.js
@@ -3,8 +3,13 @@ import Swiper from 'swiper';
 export function createSwiper() {
     const swiper = new Swiper('.swiper', {
         slidesPerView: 1,
-        spaceBetween: 10,
+        spaceBetween: 30,
         breakpoints: {
+            1921: {
+                slidesPerView: 5,
+                spaceBetween: 10,
+                centeredSlides: true,
+            },
             479: {
                 slidesPerView: 4,
                 spaceBetween: 10,
