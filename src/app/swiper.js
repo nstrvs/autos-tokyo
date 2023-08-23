91f912b2f009b799fb8c8ea7deeba14fd50fa03a
Nestor Valles <nestor@vlue.company>
2023-08-23 15:29:01 -0600

    swiper debugging

--- src/app/swiper.js
+++ src/app/swiper.js
@@ -2,6 +2,7 @@ import Swiper from 'swiper';
 
 export function createSwiper() {
     const swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
         slidesPerView: 4,
         spaceBetween: 10,
         centeredSlides: true,
