9934e032752420056fa67c2fe6aec57728eca1f7
Nestor Valles <nestor@vlue.company>
2023-08-23 15:26:13 -0600

    swiper debugging

--- src/app/swiper.js
+++ src/app/swiper.js
@@ -1,9 +1,12 @@
 import Swiper from 'swiper';
 
 export function createSwiper() {
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    const spaceBetweenVW = vw * 0.02; // 2vw
+
     const swiper = new Swiper('.swiper', {
         slidesPerView: 4,
-        spaceBetween: 30,
+        spaceBetween: spaceBetweenVW,
         centeredSlides: true,
     });
 
