c8b2c175acf1b7e3b958464d1521aed4f01b4481
Nestor Valles <nestor@vlue.company>
2023-08-23 10:38:50 -0600

    swiper debugging

--- src/app/swiper.js
+++ src/app/swiper.js
@@ -4,5 +4,7 @@ import Swiper from 'swiper';
 export function createSwiper() {
     return new Swiper('.swiper', {
         speed: 400,
+        slidesPerView: 1,
+        spaceBetween: 10,
     });
 }
