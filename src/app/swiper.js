4c3a8e965453edfce26c84ca4d2673450cb39830
Nestor Valles <nestor@vlue.company>
2023-08-23 16:06:12 -0600

    swiper debugging

--- src/app/swiper.js
+++ src/app/swiper.js
@@ -2,17 +2,13 @@ import Swiper from 'swiper';
 
 export function createSwiper() {
     const swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
         slidesPerView: 4,
-        spaceBetween: 30,
+        spaceBetween: 10,
         centeredSlides: true,
-        breakpoints: {
-            // when window width is <= 478px
-            478: {
-                slidesPerView: 1, // change this to your desired value
-                spaceBetween: 10   // change this to your desired value
-            }
-        }
     });
+
+    console.log('swiper created');
 
     return swiper;
 }
