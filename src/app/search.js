22af9f27dbc5cd3b52c9523db2dc873276352cf6
Nestor Valles <nestor@vlue.company>
2023-08-31 15:42:04 -0500

    seach function debugging

--- src/app/search.js
+++ src/app/search.js
@@ -2,6 +2,7 @@ export function search() {
     let searchLink = document.querySelectorAll('.search-link');
 
     searchLink.forEach(function (link) {
+        console.log(link);
         link.addEventListener('click', function (e) {
             let searchName = e.target.getAttribute('search');
             console.log(searchName);
