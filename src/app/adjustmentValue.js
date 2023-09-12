ea05bed04c4eac40e0ab2d67d6f6404cebc7775b
Nestor Valles <nestor@vlue.company>
2023-09-12 17:33:21 -0500

    av debugging

--- src/app/adjustmentValue.js
+++ src/app/adjustmentValue.js
@@ -2,10 +2,12 @@ export default function adjustmentValue () {
     const selectors = document.querySelectorAll('#av-1, #av-2, #av-3');
     selectors.forEach(function (selector) { 
         if (selector.textContent < 0) {
-            document.getElementById(`card-${selector.id}`).classList.add('is-negative');
+            console.log(selector.id);
+            /*document.getElementById(`card-${selector.id}`).classList.add('is-negative');
             document.querySelector(`#card-${selector.id}.fontaswesome.is-value`).textContent = '';
             document.querySelector(`#card-${selector.id}.text-block.is-value`).textContent = '-';
             document.querySelector(`#card-${selector.id}.text-block`).style.color = '#7f1111';
+            */
 
         }
     });
